78a82e0261ed439ad70196dbd61655975231b62a
qubard <qubard@gmail.com>
2018-08-05 20:55:14 -0300

    committed src

---
+++ src/xyz/blueberrypancake/srp/ReinforcementPlugin.java
@@ -0,0 +1,21 @@
+package xyz.blueberrypancake.srp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ReinforcementPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	@Override
+	public void onLoad() {
+		
+	}
+}
